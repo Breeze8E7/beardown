3415831909f6671567d848129abeddf803aa1437
OminousShadow87 <brucewaynephil@gmail.com>
2025-05-21 15:44:27 -0700

    Setting up initial repo

---
+++ main.py
@@ -0,0 +1,5 @@
+def main():
+    print("Hello, learner! Welcome to 'Bear Down for Boots', a study tool that helps you 'bear down for midterms' and provides you with personalized feedback on your progress.")
+
+if __name__ == "__main__":
+    main()
